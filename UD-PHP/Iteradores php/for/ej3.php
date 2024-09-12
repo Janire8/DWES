694ec59acb7779777198593d82311eb09ad3bc59
2AW3 User 8 <2AW3-8@yourdomain.com>
2024-09-12 11:27:09 +0200

    ejercicio3

--- UD-PHP/Iteradores php/for/ej3.php
+++ UD-PHP/Iteradores php/for/ej3.php
@@ -5,12 +5,17 @@ Si es true, mostrar en pantalla números pares del 1 al número aleatorio menor 
 si se le ha asignado el valor FALSE a la variable pares en la pantalla tienen que aparecer los valores impares.
  */
 $pares = true;
-$i = 1;
 $maximo = rand (1,99);
-
-while ($i <= $maximo) {
-    if ($i % 2 == 0) {
-        echo ($i . "<br>");
+if ($pares = false) {
+    for ($i = 0; $i < $maximo; $i++) {
+        if ($i % 2 == 0) {
+            echo ($i . "<br>");
+        }
     }
-    $i++;
+}else {
+    for ($i = 0; $i < $maximo; $i++) {
+        if ($i % 2 != 0) {
+            echo ($i . "<br>");
+        }
+    }
 }
--- UD-PHP/Iteradores php/while/ej4.php
+++ UD-PHP/Iteradores php/while/ej4.php
@@ -3,5 +3,11 @@
 /*Realiza la suma desde un número (primerNum) hasta otro (ultimoNum)
 Con While y con For
  */
+$primerNum = 5;
+$ultimoNum = 20;
 
- ?>
+while ($i < $ultimoNum) {
+    $suma = ($primerNum + $ultimoNum);
+}
+
+?>
