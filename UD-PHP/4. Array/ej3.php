890314cdb585b291e4bbbbbd9e4afaf127c3f411
2AW3 User 8 <2AW3-8@yourdomain.com>
2024-09-13 11:25:53 +0200

    ejercicios

--- UD-PHP/4. Array/ej3.php
+++ UD-PHP/4. Array/ej3.php
@@ -1,5 +1,14 @@
-<?php 
+<?php
+$rebaja = 20;
 
+$productos = array ('adidas', 'nike', 'new balance', 'puma', 'rebook');
 
+$precios = array (150, 120, 80, 90, 70);
 
+$resultado = array_combine($productos, $precios);
+
+print_r($resultado);
+for ($i=0; $i < count($precios); $i++) { 
+    $resultadoRebaja = (($rebaja * $precios)/100);
+}
 ?>
