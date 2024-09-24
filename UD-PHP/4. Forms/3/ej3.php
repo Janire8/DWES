6100c6156dc684694eb64dd777f4d0bc1301a311
2AW3 User 8 <2AW3-8@yourdomain.com>
2024-09-24 10:29:20 +0200

    ejercicios

--- UD-PHP/4. Forms/3/ej3.php
+++ UD-PHP/4. Forms/3/ej3.php
@@ -4,27 +4,41 @@ $numero1 = $_POST["num1"];
 $numero2 = $_POST["num2"];
 $btnSuma = $_POST["suma"];
 $btnResta = $_POST["resta"];
-$btnMultiplicacion = $_POST["multiplicacion"];
+$btnMultiplicacion = $_POST["multi"];
+$btnDivision = $_POST["division"];
 
-if($btnSuma) {
-    if(!empty(($_POST["num1"]) . ($_POST["num2"]))) {
-    $suma = $numero1 + $numero2;
-    echo ("La suma es : " . $suma); 
-}else {
-    echo ('<p style="color:red" No se han introducido valores</p><br>');
-}
-}
-
-
-if($btnResta) {
-    if(!empty(($_POST["num1"]) . ($_POST["num2"]))) {
-    $resta = $numero1 - $numero2;
-    echo ("La resta es : " . $resta); 
-}else {
-    echo ('<p style="color:red" No se han introducido valores</p><br>');
-}
+if ($btnSuma) {
+    if (!empty(($_POST["num1"]) . ($_POST["num2"]))) {
+        $suma = $numero1 + $numero2;
+        echo ("La suma es : " . $suma);
+    } else {
+        echo ('<p style="color:red" No se han introducido valores</p><br>');
+    }
 }
 
+if ($btnResta) {
+    if (!empty(($_POST["num1"]) . ($_POST["num2"]))) {
+        $resta = $numero1 - $numero2;
+        echo ("La resta es : " . $resta);
+    } else {
+        echo ('<p style="color:red" No se han introducido valores</p><br>');
+    }
+}
 
+if ($btnMultiplicacion) {
+    if (!empty(($_POST["num1"]) . ($_POST["num2"]))) {
+        $multiplicacion = $numero1 * $numero2;
+        echo ("La resta es : " . $multiplicacion);
+    } else {
+        echo ('<p style="color:red" No se han introducido valores</p><br>');
+    }
+}
 
-?>
+if ($btnDivision) {
+    if (!empty(($_POST["num1"]) . ($_POST["num2"]))) {
+        $division = $numero1 / $numero2;
+        echo ("La resta es : " . $division);
+    } else {
+        echo ('<p style="color:red" No se han introducido valores</p><br>');
+    }
+}
