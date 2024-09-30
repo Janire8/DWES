1a10f8914313905df8001ce98a3a6a5d0af8d9f5
2AW3 User 8 <2AW3-8@yourdomain.com>
2024-09-30 11:28:52 +0200

    poo

---
+++ UD2-BBDD/POO/Ejercicio 1/Personaje.php
@@ -0,0 +1,26 @@
+<?php 
+class Personaje {
+
+    public $nombre;
+    public $puntosDeVida;
+    public $fuerza;
+
+    public function __construct($nombre, $puntosDeVida, $fuerza) {
+
+        $this->nombre = $nombre;
+        $this->puntosDeVida = $puntosDeVida;
+        $this->fuerza = $fuerza;
+        
+    }
+}
+
+$personaje1 = new Personaje("Mario", 12, 5);
+var_dump($personaje1->nombre);
+var_dump($personaje1->puntosDeVida);
+var_dump($personaje1->fuerza);
+
+class mario extends personaje {
+
+}
+
+?>
